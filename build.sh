c154be430d3a3c22c064f988d84a3bb9023140d8
Philip Giuliani <philip.giuliani@keepinmind.info>
2025-09-26 22:36:54 +0200

    Add ls

--- build.sh
+++ build.sh
@@ -36,7 +36,9 @@ cat <<EOF >"$BUILD_SCRIPT"
     git clone --filter=blob:none --branch='$GIT_BRANCH' '$FFMPEG_REPO' ffmpeg
     cd ffmpeg
 
-    git apply $ROOT_DIR/patches/ffmpeg/ffmpeg-20342.patch
+    ls -la
+
+    git apply /patches/ffmpeg/ffmpeg-20342.patch
 
     ./configure --prefix=/ffbuild/prefix --pkg-config-flags="--static" \$FFBUILD_TARGET_FLAGS \$FF_CONFIGURE \
         --extra-cflags="\$FF_CFLAGS" --extra-cxxflags="\$FF_CXXFLAGS" --extra-libs="\$FF_LIBS" \
